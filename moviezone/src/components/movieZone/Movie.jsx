64c31452ac54b7f2d3eae6142a08a857889b9272
Priyanshi Shah <shahpriyanshi1025@gmail.com>
2025-06-01 12:05:31 +0530

    media queries added

--- moviezone/src/components/movieZone/Movie.jsx
+++ moviezone/src/components/movieZone/Movie.jsx
@@ -10,19 +10,20 @@ const Movie = () => {
     return (
         <>
             {/* <h1 className='text-center'>Movie</h1> */}
-            <div className='btns text-center my-4'>
-                <button onClick={()=>{setMovieList(movies)}} type="button" className="btn btn-outline-secondary">All</button>
-                <button onClick={()=>{filterCategory('Action')}} type="button" className="btn btn-outline-primary">Action</button>
-                <button onClick={()=>{filterCategory('Thriller')}} type="button" className="btn btn-outline-warning">Thriller</button>
-                <button onClick={()=>{filterCategory('Animation')}} type="button" className="btn btn-outline-success">Animation</button>
-                <button onClick={()=>{filterCategory('Horror')}} type="button" className="btn btn-outline-danger">Horror</button>
-                <button onClick={()=>{filterCategory('Drama')}} type="button" className="btn btn-outline-light">Drama</button>
-                <button onClick={()=>{filterCategory('Sci-Fi')}} type="button" className="btn btn-outline-info">Sci-Fi</button>
-            </div>
             <div className='main-container'>
+                <div className='btns text-center my-4'>
+                    <button onClick={() => { setMovieList(movies) }} type="button" className="btn btn-outline-secondary">All</button>
+                    <button onClick={() => { filterCategory('Action') }} type="button" className="btn btn-outline-primary">Action</button>
+                    <button onClick={() => { filterCategory('Thriller') }} type="button" className="btn btn-outline-warning">Thriller</button>
+                    <button onClick={() => { filterCategory('Animation') }} type="button" className="btn btn-outline-success">Animation</button>
+                    <button onClick={() => { filterCategory('Horror') }} type="button" className="btn btn-outline-danger">Horror</button>
+                    <button onClick={() => { filterCategory('Drama') }} type="button" className="btn btn-outline-light">Drama</button>
+                    <button onClick={() => { filterCategory('Sci-Fi') }} type="button" className="btn btn-outline-info">Sci-Fi</button>
+                </div>
+
                 <div className='flex-container'>
                     {movieList.map((data) =>
-                        <div style={{ maxWidth: '250px' }} key={data.id}>
+                        <div style={{ maxWidth: '200px' }} key={data.id}>
                             <img src={data.poster_path} alt="poster" className='poster' style={{ width: '200px' }} />
                             <p className='movie-title mt-3'>{data.title}</p>
                             <span className='release-date'>{data.release_date}</span>
